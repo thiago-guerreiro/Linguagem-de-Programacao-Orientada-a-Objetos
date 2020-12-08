150066ed7837a407bde9f33e61ce22e47e59c3a3
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2020-12-08 12:46:55 -0300

    Exceções, Try Catch

---
+++ Excecao/TesteConta.java
@@ -0,0 +1,14 @@
+public class TesteConta {
+
+	public static void main(String[] args) {
+		
+		ContaCorrente c1 = new ContaCorrente("José","123");
+		System.out.println(c1.toString());
+		
+		ContaCorrente c2 = new ContaCorrente("Maria","4567");
+		System.out.println(c2.toString());
+		
+		c1.deposito(100);
+		c1.tranferencia(100, c2);
+	}
+}
